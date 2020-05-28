4562ed57dacb5b996e3fa1c4cf38a9b6f70f1458
Rick Beyer <beyer.richard73@gmail.com>
2020-05-27 23:08:06 -0500

    changed ratings to show contact on left, power on right

--- js/index.js
+++ js/index.js
@@ -1,40 +1,70 @@
-let rating_CON_VSL = 65;
-let rating_CON_VSR = 97;
+let rating_CON_VSL = 40;
+let rating_CON_VSR = 37;
+let rating_POW_VSL = 97;
+let rating_POW_VSR = 99;
 
 let PI = 3.14159265359;
 
-const circle_vsl = document.querySelector('.blue-rating');
-const circle_vsr = document.querySelector('.blue-rating-vsr');
-const track = document.querySelector('.rating-track');
+const con_vsl = document.querySelector('.rating-con-vsl');
+const con_vsr = document.querySelector('.rating-con-vsr');
+const track = document.querySelector('.rating-track-vsl');
 const track_vsr = document.querySelector('.rating-track-vsr');
-const radius = circle_vsl.r.baseVal.value;
+const track_pow_vsl = document.querySelector('.rating-track-pow-vsl');
+const pow_vsl = document.querySelector('.rating-pow-vsl');
+const track_pow_vsr = document.querySelector('.rating-track-pow-vsr');
+const pow_vsr = document.querySelector('.rating-pow-vsr');
+
+const radius = con_vsl.r.baseVal.value;
 const circumference = radius * 2 * PI;
+
+console.log(circumference);
 
 // set vs-left-contact text
 let vslContactText = document.querySelector('.vs-left-contact');
 let vsrContactText = document.querySelector('.vs-right-contact');
-vslContactText.innerHTML = rating_CON_VSL;
-vsrContactText.innerHTML = rating_CON_VSR;
+vslContactText.innerHTML = "vs. Left";
+vsrContactText.innerHTML = "vs. Right";
 
-circle_vsl.style.strokeDasharray =  `${circumference} ${circumference}`;
-circle_vsl.style.strokeDashoffset = circumference;
+con_vsl.style.strokeDasharray =  `${circumference} ${circumference}`;
+con_vsl.style.strokeDashoffset = circumference;
 
-circle_vsr.style.strokeDasharray =  `${circumference} ${circumference}`;
-circle_vsr.style.strokeDashoffset = circumference;
+con_vsr.style.strokeDasharray =  `${circumference} ${circumference}`;
+con_vsr.style.strokeDashoffset = circumference;
 
 track.style.strokeDasharray =  `${circumference} ${circumference}`;
-track.style.strokeDashoffset =  circumference;
+track.style.strokeDashoffset =  circumference - (.4 * circumference);
+
+track_vsr.style.strokeDasharray = `${circumference} ${circumference}`;
+track_vsr.style.strokeDashoffset = circumference - (.4 * circumference);
+
+track_pow_vsl.style.strokeDasharray = `${circumference} ${circumference}`;
+track_pow_vsl.style.strokeDashoffset = circumference - (.4 * circumference);
+
+track_pow_vsr.style.strokeDasharray = `${circumference} ${circumference}`;
+track_pow_vsr.style.strokeDashoffset = circumference - (.4 * circumference);
+
+pow_vsl.style.strokeDasharray = `${circumference} ${circumference}`
+pow_vsl.style.strokeDashoffset = circumference;
+
+pow_vsr.style.strokeDasharray = `${circumference} ${circumference}`
+pow_vsr.style.strokeDashoffset = circumference;
 
 function setProgress(percent) {
-    circle_vsl.style.transition = 'stroke-dashoffset .5s ease-out';
-    circle_vsl.setAttribute('stroke',"lime"); //#0FA4F8");
-    let offset = circumference - (rating_CON_VSL * 8) / 1000 * circumference;
-    circle_vsl.style.strokeDashoffset = offset;
+    con_vsl.style.transition = 'stroke-dashoffset .5s ease-out';
+    let offset = circumference - (rating_CON_VSL * 4) / 1000 * circumference;
+    con_vsl.style.strokeDashoffset = offset;
 
-    circle_vsr.style.transition = 'stroke-dashoffset .5s .2s ease-out';
-    circle_vsr.setAttribute('stroke',"orangered"); //url(#linear)
-    offset = circumference - (rating_CON_VSR * 8) / 1000 * circumference;
-    circle_vsr.style.strokeDashoffset = offset;
+    pow_vsl.style.transition = 'stroke-dashoffset .5s .25s ease-out';
+    offset = circumference - (rating_POW_VSL * 4) / 1000 * circumference;
+    pow_vsl.style.strokeDashoffset = offset;
+
+    con_vsr.style.transition = 'stroke-dashoffset .5s .2s ease-out';
+    offset = circumference - (rating_CON_VSR * 4) / 1000 * circumference;
+    con_vsr.style.strokeDashoffset = offset;
+
+    pow_vsr.style.transition = 'stroke-dashoffset .5s .25s ease-out';
+    offset = circumference - (rating_POW_VSR * 4) / 1000 * circumference;
+    pow_vsr.style.strokeDashoffset = offset;
 }
 
 setTimeout(setProgress);
